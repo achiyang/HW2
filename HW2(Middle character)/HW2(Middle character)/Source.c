0b2e7b86ec8f0a8db38b507378f0123ed4252ed2
achi_yang <sinhouse2@gmail.com>
2023-09-11 23:28:08 +0900

    HW2

--- HW2(Middle character)/HW2(Middle character)/Source.c
+++ HW2(Middle character)/HW2(Middle character)/Source.c
@@ -1,1 +1,16 @@
 #include <stdio.h>
+#include <string.h>
+
+char get_mid_char(char* str) {
+	return str[(strlen(str) + 1) / 2 - 1];
+}
+
+int main() {
+	char string[100];
+
+	scanf("%s", string);
+
+	printf("%c\n", get_mid_char(string));
+
+	return 0;
+}
