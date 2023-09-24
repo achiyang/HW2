a8e994b55fda7e2e318f8ab623db297d982f3223
achiyang <sinhouse2@gmail.com>
2023-09-24 18:56:13 +0900

    ^Q^~

---
+++ P4(echo)/Source.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void print_echo(int len, char* str) {
+	for (int i = 0; i < len; i++) {
+		for (int j = 0; j < 2; j++) {
+			printf("%c", str[i]);
+		}
+	}
+	return;
+}
+
+int main() {
+	int N;
+	char S[51];
+
+	scanf("%d %s", &N, S);
+
+	print_echo(N, S);
+
+	return 0;
+}
