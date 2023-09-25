2c3eb0959ca9da3576fe07c0e762aa90ec93e3bd
achiyang <130957543+achiyang@users.noreply.github.com>
2023-09-25 15:38:56 +0900

    Create Sourse_.c

---
+++ P1(substr)/Sourse_.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+void get_substr(int* start, int* end) {
+	const char* str = "beautiful";
+	char substr[10];
+	int sublen = *end - *start + 1;
+
+	strncpy(substr, str + *start - 1, sublen);
+
+	substr[sublen] = '\0';
+
+	printf("%s", substr);
+
+	return;
+}
+
+int main() {
+	int L, R;
+
+	scanf("%d %d", &L, &R);
+	get_substr(&L, &R);
+
+	return 0;
+}
