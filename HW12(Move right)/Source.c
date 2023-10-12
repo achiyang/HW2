29c6afd8a8b8c4309039b9cc80543cc220a01fe1
achiyang <sinhouse2@gmail.com>
2023-10-12 11:40:09 +0900

    ^Q^~

--- HW12(Move right)/Source.c
+++ HW12(Move right)/Source.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 
 void move_right(char* S) {
-    for (int i = 0; i < 3; i++) {
-        S[3 - i] = S[2 - i];
-    }
-    S[0] = '0';
+
 }
 
 int main() {
-    char S[5]; // 문자열 S, 길이는 4 + NULL 문자('\0') = 5
+    char S[5];
 
     scanf("%s", S);
     move_right(S);
