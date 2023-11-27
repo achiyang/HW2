0b34aff5a5f4ab31fc46084f2eb24a008f9c2814
achiyang <sinhouse2@gmail.com>
2023-11-27 15:27:11 +0900

    ^Q^~

--- P11(password)/Source.c
+++ P11(password)/Source.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
 
-int main() {
-	char password[5];
-
-	scanf("%s", password);
-
+int isPasswordWeak(char* password) {
 	int is_weak = 1;
+
 	for (int i = 0; i < 3; i++) {
 		if (password[i] != password[i + 1]) {
 			is_weak = 0;
@@ -16,14 +13,22 @@ int main() {
 	if (!is_weak) {
 		is_weak = 1;
 		for (int i = 0; i < 3; i++) {
-			if ((password[i] - '0' + 1) % 10 != password[i + 1] - '0') {
+			if (((password[i] - '0') == 9 ? 0 : password[i] - '0' + 1) != password[i + 1] - '0') {
 				is_weak = 0;
 				break;
 			}
 		}
 	}
 
-	if (is_weak) {
+	return is_weak;
+}
+
+int main() {
+	char password[5];
+
+	scanf("%s", password);
+
+	if (isPasswordWeak(password)) {
 		printf("Weak\n");
 	}
 	else {
